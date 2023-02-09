0f6c220e029c25ff68521c160406a8cf9e787833
Malabeadman <deangelo.lee19@gmail.com>
2023-02-08 23:44:29 -0500

    js to typescript

--- DeASite/src/app/projects/projects.component.ts
+++ DeASite/src/app/projects/projects.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./projects.component.scss']
 })
 export class ProjectsComponent implements OnInit {
-
+/// start to change the js into typescript
   constructor() { }
 
   ngOnInit(): void {
